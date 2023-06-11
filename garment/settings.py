f07ba1f7d54b9bec3b42b5265d4a74a36ab97a42
Eazel <eazeldonf1@yahoo.com>
2023-06-11 18:32:38 +0200

    Update setting.py

--- garment/settings.py
+++ garment/settings.py
@@ -141,7 +141,7 @@ AUTH_USER_MODEL = 'accounts.Account'
 #__________________________
 DATABASES = {
     'default': dj_database_url.config(         
-           conn_max_age=600  
+           conn_max_age=600, 
            conn_health_checks=True,  
            ),
 
