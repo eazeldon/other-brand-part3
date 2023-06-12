24ee41da78d5be9d0a12588766caabe9e9699276
Eazel <eazeldonf1@yahoo.com>
2023-06-12 02:55:55 +0200

    Update .env

--- garment/settings.py
+++ garment/settings.py
@@ -177,7 +177,7 @@ else:
 '''  
 #------------------
 
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
