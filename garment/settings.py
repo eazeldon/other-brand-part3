b5b615ef3546279b7c1001958d5f948c7b1436a0
Eazel <eazeldonf1@yahoo.com>
2023-06-13 17:41:13 +0200

    Add database

--- garment/settings.py
+++ garment/settings.py
@@ -169,11 +169,11 @@ if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(env('DATABASE_URL')),
            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ('RDS_DB_NAME'),
-            'USER': os.environ('RDS_DB_USERNAME'),
-            'PASSWORD': os.environ('RDS_DB_PASSWORD'),
-            'HOST': os.environ('RDS_DB_HOSTNAME'),
-            'PORT': os.environ('RDS_DB_PORT'),
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_DB_USERNAME'],
+            'PASSWORD': os.environ['RDS_DB_PASSWORD'],
+            'HOST': os.environ['RDS_DB_HOSTNAME'],
+            'PORT': os.environ['RDS_DB_PORT'],
         }
     
 
